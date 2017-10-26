cf5a467080071cd11a3cdbb62e91485254a3acdd
itsmesinghavneet <32716672+itsmesinghavneet@users.noreply.github.com>
2017-10-26 11:20:16 +0530

    Update ember_test.py

--- ember_test.py
+++ ember_test.py
@@ -10,7 +10,7 @@ import unittest, time, re
 
 class Testember(unittest.TestCase):
     def setUp(self):
-        phantomjsPath="/var/lib/phantomjsdriver"
+        phantomjsPath="/usr/bin/phantomjs"
         self.driver = webdriver.PhantomJS(executable_path=phantomjsPath)
         #firefoxPath="/usr/local/bin/geckodriver"
         #self.driver = webdriver.Firefox(executable_path=firefoxPath)
